8227bd26681f22940184cf86f57cd552336bb8b2
niklasschwetke <77964955+niklasschwetke@users.noreply.github.com>
2022-09-28 17:56:06 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace zbuffer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start transformation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(165, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            370,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "number of images";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(165, 75);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "threshold";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(29, 75);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(112, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(165, 114);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            359,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "scan angle";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 154);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "contrast adjustment";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "RESET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(51, 177);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(181, 177);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 11;
+            this.trackBar2.Value = 255;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "min";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(162, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "max";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(162, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "255";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(165, 154);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 17);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "automatic";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(301, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "_________________________________________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 234);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "zBuffer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LS_MIP;
+
+namespace zbuffer
+{
+    public partial class Form1 : Form
+    {
+        LSimage outputImage;
+
+        public int z ; // Nummer der Bilder   
+        public byte T ;  // Threshold
+        public double phi; //Startwinkel in radian
+        public int phidegree; //Startwinkel in grad
+        public byte B, C;
+        public int[,] array;
+        public int[,] array2;
+        public bool loaded;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "zBuffer"; 
+            outputImage = new LSimage("G:\\MIP\\Black.bmp", "zBuffer");
+
+            loaded = false;
+        }
+
+        private unsafe void button1_Click(object sender, EventArgs e)
+        {
+            z = (int) numericUpDown1.Value; // Nummer der Bilder   
+            T = (byte) numericUpDown2.Value;  // Threshold
+            phidegree = (int)numericUpDown3.Value; //Startwinkel
+            progressBar1.Maximum = z;
+            loaded = true;
+            array = new int[512, z];
+            array2 = new int[512, z];
+
+
+            /* ----------code für mehrmalige bilder
+            phidegree = 0;
+            int phiend = 360;
+            int phistep = 1;
+
+            T = 20;
+            int Tend = 220;
+
+            //while (phidegree < phiend) //bilder nach winkel
+            while (T < Tend) //bilder nach threshold
+            {
+            */ //-------------
+
+
+                phi = (phidegree * Math.PI / 180.0);
+
+                for (int zz = 1; zz < z + 1; zz++) //Bilder öffnen nacheinander
+                {
+                    Bitmap bmp = new Bitmap("G:\\MIP\\SlicesInBMP\\IM-0001-0" + Convert.ToString(zz) + ".bmp");
+                    BitmapData bData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+                    byte* scan0 = (byte*)bData.Scan0; //pointer, der auf die bits der bitmap zeigt
+
+                    Bitmap bmpr = new Bitmap("G:\\MIP\\SlicesInBMPr\\IM-0001-0" + Convert.ToString(zz) + ".bmp");
+                    BitmapData bDatar = bmpr.LockBits(new Rectangle(0, 0, bmpr.Width, bmpr.Height), ImageLockMode.ReadWrite, bmpr.PixelFormat);
+                    byte* scan0r = (byte*)bDatar.Scan0;
+
+                    if (phi != 0)
+                    {
+                        int cxi = (int)((bmp.Width - 1) / 2.0); //die Bildmitten ermitteln
+                        int cyi = (int)((bmp.Height - 1) / 2.0);
+                        int cxo = (int)((bmpr.Width - 1) / 2.0);
+                        int cyo = (int)((bmpr.Height - 1) / 2.0);
+
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            for (int y = 0; y < bmp.Height; y++)
+                            {
+                                scan0r[bmpr.Width * y + x] = 0;
+                            }
+                        }
+
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            for (int y = 0; y < bmp.Height; y++)
+                            {
+                                int xr = (int)((x - cxo) * Math.Cos(phi) + (y - cyo) * Math.Sin(phi) + cxi); // Rotationsformeln für das Bild um den Mittelpunkt
+                                int yr = (int)(-(x - cxo) * Math.Sin(phi) + (y - cyo) * Math.Cos(phi) + cyi);
+
+                                if (xr >= 0 && yr >= 0 && xr < bmp.Width && yr < bmp.Height)
+                                {
+                                    scan0r[bmpr.Width * yr + xr] = scan0[bmp.Width * y + x];
+                                }
+                            }
+                        }
+                    }
+
+                    for (int x = 0; x < bmpr.Width; x++)
+                    {
+                        int i = 0;
+                        int i2 = 0;
+                        int y = 0;
+
+                        while (i2 == 0 & y < bmpr.Height) //überprüfe jeden nten y Wert bis der Körperbereich erreicht ist, zur Beschleunigung des Programmes
+                        {
+                            B = scan0r[bmpr.Width * y + x];
+
+                            if (B == 0)
+                            {
+                                y += 10;
+                            }
+
+                            else
+                            {
+                                while (i2 == 0) //gehe rückwärts, bis erster körperpixel erreicht ist
+                                {
+                                    y--;
+                                    C = scan0r[bmpr.Width * y + x];
+
+                                    if (C == 0)
+                                    {
+                                        i2++;
+                                    }
+                                }
+                            }
+                        }
+
+                        while (i == 0 & y < bmpr.Height) //führe Suche nach Pixel über Threshold im Körper fort
+                        {
+                            B = scan0r[bmpr.Width * y + x];
+
+                            if (B > T)
+                            {
+                                byte zBuffer = (byte)((1 - ((double)y / bmpr.Height)) * 255.0); //errechnet zBuffer Wert, abhängig von der Höhe der Bilder
+                                Color Tiefe = Color.FromArgb(zBuffer, zBuffer, zBuffer);
+                                outputImage.bitmap.SetPixel(x, zz - 1, Tiefe);
+                                array[x, zz - 1] = y; // speichert die Tiefen Information der Pixel über dem Threshold für spätere Bearbeitung in einem Array
+                                array2[x, zz - 1] = zBuffer; //speichert zBuffer werte in array2                        
+                                i++;
+                            }
+
+                            else
+                            {
+                                y++;
+
+                            }
+                        }
+                    }
+                    bmpr.UnlockBits(bData);
+                    bmpr.Dispose();
+                    bmp.UnlockBits(bData);
+                    bmp.Dispose();
+                    outputImage.UpdateScreen();
+                    progressBar1.PerformStep();
+                }
+
+            /* --------code für mehrmalige bilder
+            //outputImage.bitmap.Save("C:\\Users\\nikla\\Desktop\\zbuffer\\" + Convert.ToString(phidegree) + ".bmp"); //winkel
+            outputImage.bitmap.Save("C:\\Users\\nikla\\Desktop\\zbufferT\\" + Convert.ToString(T) + ".bmp"); //threshold
+
+            Array.Clear(array, 0, array.Length);
+            Array.Clear(array2, 0, array2.Length);
+            progressBar1.Value = 0;
+            for (int x = 0; x < outputImage.bitmap.Width; x++)
+            {
+                for (int y = 0; y < outputImage.bitmap.Height; y++)
+                {
+                    Color black = Color.FromArgb(0, 0, 0);
+                    outputImage.bitmap.SetPixel(x, y, black);
+                }
+            }
+            outputImage.UpdateScreen();
+            */ //------------
+
+            outputImage.bitmap.Save("C:\\Users\\nikla\\Desktop\\nativ.bmp");
+
+
+            if (checkBox1.Checked == true)
+                {
+                    Kontrast();
+                }
+
+
+            outputImage.bitmap.Save("C:\\Users\\nikla\\Desktop\\autokontrast.bmp");
+
+            /*---------code für mehrmalige bilder
+            //phidegree += phistep;
+            T++;
+        }
+        */ //----------------
+        }
+
+
+        private void button2_Click(object sender, EventArgs e) //setzt alles zurück
+        {
+            Array.Clear(array, 0, array.Length);
+            Array.Clear(array2, 0, array2.Length);
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            trackBar1.Maximum = 255;
+            trackBar1.Value = 0;
+            trackBar2.Minimum = 0;
+            trackBar2.Value = 255;
+            label6.Text = Convert.ToString(trackBar1.Value);
+            label7.Text = Convert.ToString(trackBar2.Value);
+            progressBar1.Value = 0;
+            loaded = false;
+
+            for (int x = 0; x < outputImage.bitmap.Width; x++)
+            {
+                for (int y = 0; y < outputImage.bitmap.Height; y++)
+                {
+                    Color black = Color.FromArgb( 0 , 0 , 0 );
+                    outputImage.bitmap.SetPixel(x, y, black);
+                }
+            }
+            outputImage.UpdateScreen();
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = Convert.ToString(trackBar1.Value);
+            trackBar2.Minimum = trackBar1.Value;
+
+            if (loaded == true)
+            {
+                Kontrast();
+            }
+        }
+
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label7.Text = Convert.ToString(trackBar2.Value);
+            trackBar1.Maximum = trackBar2.Value;
+
+            if (loaded == true)
+            {
+                Kontrast();
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loaded == true)
+            {
+                Kontrast();
+            }
+        }
+
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loaded == true)
+            {
+                Kontrast();
+            }
+        }
+
+
+        private void Kontrast() //kontrastanpassung
+        {
+            if (checkBox1.Checked == true) 
+            {
+                int max = 0;
+                int min = 512;
+
+                if (checkBox2.Checked == true) //automatisch
+                {
+
+                    for (int x = 0; x < 512; x++) //findet minimalen und maximalen pixel wert, ignoriert den Background
+                    {
+                        for (int y = 0; y < z; y++)
+                        {
+                            int a = array[x, y];
+
+                            if (a > max && a != 0)
+                            {
+                                max = a;
+                            }
+
+                            if (a < min && a != 0)
+                            {
+                                min = a;
+                            }
+                        }
+                    }
+                }
+
+                if (checkBox2.Checked == false) //manuell
+                {
+                    min =(int) (trackBar1.Value / 255.0 * 512);
+                    max =(int) (trackBar2.Value / 255.0 * 512);
+                }
+
+                for (int x = 0; x < 512; x++)
+                {
+                    for (int y = 0; y < z; y++)
+                    {
+                        int d = max - min; //bereich der validen Pixel/Körperpixel
+                        int kontrast = array[x, y];
+
+                        if (kontrast != 0)  //background ignorieren
+                        {                            
+                            byte zBuffer2 = (byte)((1 - ((double)(kontrast - min) / d)) * 255.0); //skaliert einen neuen zBuffer Wert abhängig von maximalen und minimalen zBuffer Werten
+
+                            if (kontrast <= min)
+                            {
+                                zBuffer2 = 255;
+                            }
+
+                            if (kontrast >= max)
+                            {
+                                zBuffer2 = 0;
+                            }
+
+                            Color Kontrast = Color.FromArgb(zBuffer2, zBuffer2, zBuffer2);
+                            outputImage.bitmap.SetPixel(x, y, Kontrast);
+                        }
+                    }
+                }
+                outputImage.UpdateScreen();
+            }
+
+
+            if (checkBox1.Checked == false) // altes bild laden (ohne kontrastanpassung)
+            {
+                for (int x = 0; x < outputImage.bitmap.Width; x++)
+                {
+                    for (int y = 0; y < outputImage.bitmap.Height; y++)
+                    {
+                        int altewerte = array2[x, y];
+
+                        if (altewerte != 0)  //background ignorieren
+                        {
+                            Color Old = Color.FromArgb(altewerte, altewerte, altewerte);
+                            outputImage.bitmap.SetPixel(x, y, Old);
+                        }
+
+                    }
+                }
+                outputImage.UpdateScreen();
+            }
+        }
+
+    }
+}
